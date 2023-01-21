63ce0ae2844cfe089c411c5452e53ee7c3bd14a8
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2023-01-21 20:09:44 -0300

    create Project

---
+++ Calculator-UnitTests/Calculator-UnitTestsUITests/Calculator_UnitTestsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Calculator_UnitTestsUITestsLaunchTests.swift
+//  Calculator-UnitTestsUITests
+//
+//  Created by Kaique Lopes on 21/01/23.
+//
+
+import XCTest
+
+final class Calculator_UnitTestsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
