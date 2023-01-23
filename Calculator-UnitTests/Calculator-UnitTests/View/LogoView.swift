beec143f2034505e4ceaebc448d50a322ea25708
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2023-01-22 22:37:14 -0300

    create constraints

--- Calculator-UnitTests/Calculator-UnitTests/View/LogoView.swift
+++ Calculator-UnitTests/Calculator-UnitTests/View/LogoView.swift
@@ -9,7 +9,7 @@ import UIKit
 import SnapKit
 
 class LogoView: UIView {
-    
+    // MARK: - Componentes
     private lazy var imageView: UIImageView = {
         let imageView = UIImageView(image: .init(named: "icCalculatorBW"))
         imageView.contentMode = .scaleAspectFit
@@ -20,25 +20,37 @@ class LogoView: UIView {
     private lazy var topLabel: UILabel = {
         let label = UILabel()
         let text = NSMutableAttributedString(
-            string: "Mr TIP",
+            string: "Super Conta",
             attributes: [.font: ThemeFont.demiBold(ofSize: 16)])
-        text.addAttributes([.font: ThemeFont.bold(ofSize: 24)], range: NSMakeRange(3, 3))
+        text.addAttributes([.font: ThemeFont.bold(ofSize: 24)], range: NSMakeRange(5, 6))
         label.attributedText = text
         return label
     }()
     
     private lazy var bottomLabel: UILabel = {
-        let label = UILabel()
-        label.text = "teste"
+        LabelFactory.build(
+            text: "Calculadora",
+            font: ThemeFont.demiBold(ofSize: 20),
+            textAlignment: .left
+        )
+    }()
+    
+    private lazy var vStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [
+            topLabel,
+            bottomLabel
+        ])
+        stackView.axis = .vertical
+        stackView.spacing = -4
+        stackView.alignment = .center
         
-        return label
+        return stackView
     }()
     
     private lazy var hStackView: UIStackView = {
         let stackView = UIStackView(arrangedSubviews: [
-        imageView,
-        topLabel,
-        bottomLabel
+            imageView,
+            vStackView
         ])
         stackView.axis = .horizontal
         stackView.spacing = 8
@@ -47,12 +59,32 @@ class LogoView: UIView {
         return stackView
     }()
     
+    // MARK: - Ciclo de Vida
     init() {
         super.init(frame: .zero)
+        setupAddSubViews()
+        setupConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - Adicionando Componentes na view
+    private func setupAddSubViews() {
+        addSubview(vStackView)
+        addSubview(hStackView)
+    }
+    
+    // MARK: - Criando Constraints
+    private func setupConstraints() {
+        hStackView.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview()
+            make.centerX.equalToSuperview()
+        }
+        
+        imageView.snp.makeConstraints { make in
+            make.height.equalTo(imageView.snp.width)
+        }
+    }
 }
