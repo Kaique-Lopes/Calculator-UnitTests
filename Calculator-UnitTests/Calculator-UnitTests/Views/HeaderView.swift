fa82455ef7118e07551c5121a762081822844c6a
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2023-02-20 22:42:46 -0300

    create headerview

--- Calculator-UnitTests/Calculator-UnitTests/Views/HeaderView.swift
+++ Calculator-UnitTests/Calculator-UnitTests/Views/HeaderView.swift
@@ -8,6 +8,40 @@
 import UIKit
 
 class HeaderView: UIView {
+    
+    private lazy var topLabel: UILabel = {
+        let label = LabelFactory.build(
+            text: nil,
+            font: ThemeFont.bold(ofSize: 18)
+        )
+        
+        return label
+    }()
+
+    private lazy var bottomLabel: UILabel = {
+        let label = LabelFactory.build(
+            text: nil,
+            font: ThemeFont.bold(ofSize: 18)
+        )
+        
+        return label
+    }()
+    
+    private let topSpacerView = UIView()
+    private let bottomSpacerView = UIView()
+    
+    private lazy var stackView: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [
+            topSpacerView,
+            topLabel,
+            bottomSpacerView,
+            bottomLabel
+        ])
+        
+        return stack
+    }()
+    
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         layout()
